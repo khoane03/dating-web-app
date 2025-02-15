a607aa87bf2ab72920844ea82581f66e4035bad8
khoane03 <khoatk4444@gmail.com>
2025-02-16 00:28:28 +0700

    base project

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,8 @@
 function Home() {
     return ( 
-        <>
-        </>
+        <div className="w-screen h-screen bg-amber-100 flex justify-center items-center">
+        <h1>Wellcome to home page</h1>
+        </div>
      );
 }
 
