5ac0c2728586ec61a4bf3660fbede10039bdcb0c
ynhiii <dangyennhi0412@gmail.com>
2025-02-17 09:50:35 +0700

    nhy

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ function Home() {
     return ( 
         <div className="w-screen h-screen bg-amber-100 flex justify-center items-center">
         <h1>Wellcome to home page</h1>
+        <h2>Nhi commit</h2>
         </div>
      );
 }
