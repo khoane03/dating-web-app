3dafff5750ec0f61ea000670c7ab89709f8b698c
khoane03 <khoatk4444@gmail.com>
2025-02-26 00:12:38 +0700

    fix business login and register

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { authRouter } from './router/authRouter.js';
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.use('/auth', authRouter);
 
--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,5 +1,13 @@
 import apiResponse from "../utils/apiResponse.js";
-import { login as loginService, register as registerService } from "../service/authService.js";
+import {
+  login as loginService,
+  register as registerService,
+  sendOtp as sendOtpService,
+  verifiyOtp as verifiyOtpService,
+  refreshToken as refreshTokenService,
+  handleGoogleCallback as googleLoginCallbackService
+} from "../service/authService.js";
+import passport from "../config/googleConfig.js";
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
@@ -7,8 +15,43 @@ export const login = async (req, res) => {
   return apiResponse(res, result.code, result.message, result.data);
 };
 
+export const googleLogin = passport.authenticate("google", {
+  scope: ["profile", "email"]
+});
+
+
+export const googleCallback = async (req, res) => {
+  passport.authenticate("google", { session: false }, async (err, user) => {
+    if (err || !user) {
+      return apiResponse(res, 401, "Lỗi đăng nhập bằng Google", err);
+    }
+    const result = await googleLoginCallbackService(user.user);
+    return apiResponse(res, result.code, result.message, result.data);
+  })(req, res);
+};
+
 export const register = async (req, res) => {
   const { email, password, phone } = req.body;
   const result = await registerService(email, password, phone);
   return apiResponse(res, result.code, result.message, result.data);
 };
+
+export const refreshToken = async (req, res) => {
+  const { token } = req.body;
+  const result = await refreshTokenService(token);
+  return apiResponse(res, result.code, result.message, result.data);
+}
+
+export const sendOtp = async (req, res) => {
+  const { email } = req.body;
+  const result = await sendOtpService(email);
+  return apiResponse(res, result.code, result.message);
+}
+
+export const verifyOtp = async (req, res) => {
+  const { email, otp } = req.body;
+  const result = await verifiyOtpService(email, otp);
+  return apiResponse(res, result.code, result.message);
+}
+
+
