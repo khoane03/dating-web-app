739117fcfe4d1e98683dc3496fc7c25830a1b039
khoane03 <khoatk4444@gmail.com>
2025-02-28 23:59:01 +0700

    handle cookie

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { authRouter } from './router/authRouter.js';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
