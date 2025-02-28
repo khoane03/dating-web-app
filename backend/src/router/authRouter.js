4375c65a917c56fc96bdcb90ee3686836a75fd33
khoane03 <khoatk4444@gmail.com>
2025-02-28 23:59:44 +0700

    api forgot pass

--- backend/src/router/authRouter.js
+++ backend/src/router/authRouter.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
     login,
     register,
+    forgotPassword,
     sendOtp,
     verifyOtp,
     refreshToken,
     googleLogin, 
-    googleCallback
+    googleCallback,
+    checkEmailExist
 } from "../controller/authController.js";
 const authRouter = express.Router();
 
@@ -17,6 +19,7 @@ authRouter.post("/verify_otp", verifyOtp);
 authRouter.post("/refresh_token", refreshToken)
 authRouter.get("/google", googleLogin);
 authRouter.get("/google/callback",googleCallback);
-  
+authRouter.put("/forgot_password", forgotPassword);
+authRouter.post("/check_email", checkEmailExist);
 
 export { authRouter };
