6d250d18a375fac33c66a178214ad1fd32e7a277
SUPPERMAN\Admin <lethiquynhtrang10042003@gmail.com>
2025-03-03 11:23:04 +0700

    Fix merge conflict in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,2 @@
-import express from 'express';
-import cors from 'cors';
-import { authRouter } from './router/authRouter.js';
-import { userRouter } from './router/userRouter.js';
-
-import { authMiddleware } from './middlewares/authMiddleWare.js';
-
-import cookieParser from "cookie-parser";
-
-const app = express();
-
-app.use(cookieParser());
-app.use(express.json());
-app.use(cors());
-
-app.use('/auth', authRouter);
-
-app.use('/user', authMiddleware, userRouter);
-
-export const viteNodeApp = app;
+git add backend/src/app.js
+git commit -m "Fix merge conflict in app.js"
