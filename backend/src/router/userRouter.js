6b4b4459e0ed2efc83e3dec76b994af054685eb6
khoane03 <khoatk4444@gmail.com>
2025-03-03 10:54:39 +0700

    add middleWare and api getUserLogin

--- backend/src/router/userRouter.js
+++ backend/src/router/userRouter.js
@@ -3,4 +3,4 @@ import { getUserLogin } from "../controller/userController";
 
 export const userRouter = express.Router();
 
-userRouter.post("/info", getUserLogin);
+userRouter.get("/info", getUserLogin);
