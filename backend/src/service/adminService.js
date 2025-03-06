58bb36b6efd6a0398bf16c5c30ffe4ba9a0591f9
khoane03 <khoatk4444@gmail.com>
2025-03-06 23:06:20 +0700

    api delete Acc and Matches

--- backend/src/service/adminService.js
+++ backend/src/service/adminService.js
@@ -22,33 +22,18 @@ export const initAdmin = async () => {
 export const getAccById = async (id) => {
     try {
         const { rows } = await pool.query(`SELECT * FROM tbl_accounts WHERE id = $1`, [id]);
-        return {
-            code: 200,
-            message: 'Tài khoản',
-            data: rows[0]
-        }
+        return handleSuccess(200, 'Tài khoản', rows[0]);
     } catch (error) {
-        return {
-            code: 500,
-            message: error.message
-        }
+        return handleError(error);
     }
 };
 
 export const getAllAccounts = async () => {
     try {
         const { rows } = await pool.query(`SELECT * FROM tbl_accounts OFFSET 0 `);
-        return {
-            code: 200,
-            message: 'Danh sách tài khoản',
-            data: rows
-        }
+        return handleSuccess(200, 'Danh sách tài khoản', rows);
     } catch (error) {
-        return {
-            code: 500,
-            message: error.message
-        }
-
+        return handleError(error);
     }
 };
 
@@ -71,15 +56,54 @@ export const getAllMatches = async () => {
                         OFFSET 0 LIMIT 10;`;
 
         const { rows } = await pool.query(query);
-        return {
-            code: 200,
-            message: 'Danh sách ghép đôi',
-            data: rows
-        }
+        return handleSuccess(200, 'Danh sách matches', rows);
     } catch (error) {
-        return {
-            code: 500,
-            message: error.message
-        }
+        return handleError(error);
+    }
+}
+
+export const updatStatusAccount = async (id, status) => {
+    try {
+        const query = `UPDATE tbl_accounts SET status = $1 WHERE id = $2`;
+        await pool.query(query, [status, id]);
+        return handleSuccess(200, 'Cập nhật tài khoản thành công!');
+    } catch (error) {
+        return handleError(error);
+    }
+}; 
+
+export const deleteAccountById = async (id) => {
+    try {
+        const query = `DELETE FROM tbl_accounts WHERE id = $1`;
+        await pool.query(query, [id]);
+        return handleSuccess(200, 'Xóa tài khoản thành công');
+    } catch (error) {
+       return handleError(error);
+    }
+};
+
+export const deleteMatchById = async (id) => {
+    try {
+        const query = `DELETE FROM tbl_matches WHERE id = $1`;
+        await pool.query(query, [id]);
+        return handleSuccess(200, 'Xóa match thành công');
+    } catch (error) {
+        return handleError(error);
+    }
+};
+
+
+
+const handleSuccess = (code, message, data) => {
+    return {
+        code: code,
+        message,
+        data
+    }
+}   
+const handleError = (error) => {
+    return {
+        code: 500,
+        message: error.message
     }
 }
