f4c9290547e4be4dd37b26d3d9dd5fa283cedb4b
SUPPERMAN\Admin <lethiquynhtrang10042003@gmail.com>
2025-03-07 00:36:31 +0700

    commit

--- backend/src/service/userInfo.js
+++ backend/src/service/userInfo.js
@@ -1,6 +1,6 @@
 import pool from "../config/dbConfig";
 
-export const addProfile = async (fullName, gender, dob, occupation, bio, avatar_url, address, id, hobbies, criteria) => {
+export const addProfile = async (fullName, gender, dob, occupation, bio, avatar_url, address, id, hobbies, criteria, longitude, latitude)  => {
 
     function calculateAge(birthdate) {
         const birthDate = new Date(birthdate);
@@ -22,12 +22,27 @@ export const addProfile = async (fullName, gender, dob, occupation, bio, avatar_
 
     try {
         const query = `
-        INSERT INTO tbl_users (acc_id, full_name, gender, dob, age, occupation, hobbies, criteria, bio, avatar_url, address)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+        INSERT INTO tbl_users (acc_id, full_name, dob, gender, age, occupation, bio, avatar_url, address, created_at, criteria, hobbies, longitude, latitude)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW(), $10, $11, $12, $13)
+        ON CONFLICT (acc_id) DO UPDATE 
+        SET full_name = EXCLUDED.full_name,
+            dob = EXCLUDED.dob,
+            gender = EXCLUDED.gender,
+            age = EXCLUDED.age,
+            occupation = EXCLUDED.occupation,
+            bio = EXCLUDED.bio,
+            avatar_url = EXCLUDED.avatar_url,
+            address = EXCLUDED.address,
+            created_at = NOW(),
+            criteria = EXCLUDED.criteria,
+            hobbies = EXCLUDED.hobbies,
+            longitude = EXCLUDED.longitude,
+            latitude = EXCLUDED.latitude
         RETURNING *;
     `;
 
-        const values = [id, fullName, gender, dob, age, occupation, hobbies, criteria, bio, avatar_url, address];
+        const values = [id, fullName, dob, gender, age, occupation, bio, avatar_url, address, criteria, hobbies, longitude, latitude];
+
 
         const result = await pool.query(query, values);
 
