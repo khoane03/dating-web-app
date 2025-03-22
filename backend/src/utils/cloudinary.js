7782503deea6b078c919bce69dd1781808fd37a8
khoane03 <khoatk4444@gmail.com>
2025-03-22 17:11:55 +0700

    remove vite and fix crash app

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,4 +1,4 @@
-import { cloudinary } from "../config/cloudinaryConfig";
+import { cloudinary } from "../config/cloudinaryConfig.js";
 
 const handleCloudinaryUpload = async (file) => {
     const {secure_url} = await cloudinary.uploader.upload(file.path, {
